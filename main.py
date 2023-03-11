1e90da5b666f25c297e409ab552fb677fd0d5ae8
sva777 <vasya.sokolov@mail.ru>
2023-03-11 15:34:31 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,116 +1,37 @@
-# Задание №1
-documents = [
-    {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-    {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-    {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"}
-]
-directories = {
-    '1': ['2207 876234', '11-2'],
-    '2': ['10006'],
-    '3': []
-}
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
 
 
-def get_name_piople():
-    doc_f = input('Укажите номер документа: ')
-    rez = "Человек с указанным номера документа не найден"
-    for doc in documents:
-        if doc_f == doc['number']:
-            rez = f'Документ выдан на имя: {doc["name"]}'
-            break
-    return rez
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
 
-
-def get_num_shelf():
-    docf = input('Укажите номер документа: ')
-    rez = "Человек с указанным номера документа не найден"
-    for indx, item_doc in directories.items():
-        if docf in item_doc:
-            rez = f'Полка номер {indx}'
-    return rez
-
-
-def print_list_doc():
-    for doc in documents:
-        print(f'passport "{doc["number"]}" "{doc["name"]}"')
-    return ''
-
-
-def add_doc():
-    num = input('Укажите номер документа: ')
-    type_d = input('Укажите тип документа: ')
-    name = input('Укажите имя клиента: ')
-    num_shelf = input('Укажите номер полки: ')
-    if num_shelf not in directories.keys():
-        print("Вы указали не существующую полку")
-    else:
-        documents.append({"type": type_d, "number": num, "name": name})
-        directories[num_shelf].append(num)
-    return
-
-
-# Задание №2
-def delete_doc():
-    num = input('Укажите номер документа: ')
-    for doc in documents:
-        if num == doc['number']:
-            del doc['number']
-            break
-    for indx, num_doc in directories.items():
-        if num in num_doc:
-            del directories[indx]
-            return
-    print("Нет такого документа")
-    return ''
-
-
-def move_doc():
-    num_doc = input('Укажите номер документа: ')
-    num_shelf = input('Укажите номер полки: ')
-    if num_shelf not in directories.keys():
-        return print('Вы указали не существующую полку')
-    for indx, doc_it in directories.items():
-        if num_doc in doc_it:
-            doc_it.remove(num_doc)
-            directories[num_shelf].append(num_doc)
-            print('Документ перенесен на новую полку')
-            return
-    print('Вы указали не существующий номер документа')
-    return ''
-
-
-def add_shelf():
-    num_shelf = input('Укажите номер полки: ')
-    if str(num_shelf) in directories.keys():
-        print('Такая полка уже существует!')
-        return ''
-    directories[str(num_shelf)] = []
-
-
-def main():
-    while True:
-        command = input().lower()
-        if command == "p":
-            print(get_name_piople())
-        elif command == "s":
-            print(get_num_shelf())
-        elif command == "l":
-            print_list_doc()
-        elif command == "a":
-            add_doc()
-        elif command == "d":
-            delete_doc()
-        elif command == "m":
-            move_doc()
-        elif command == "as":
-            add_shelf()
-
-        elif command == "q":
-            print('Goodbye')
-            break
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
         else:
-            print("Неверно введена команда. Попробуйте еще раз!")
+            return 'Ошибка'
 
 
-main()
-print("Hello!")
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+
+cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+
+print(best_student.grades)
