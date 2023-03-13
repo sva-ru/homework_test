eaa01f3429076d4cc4b1ebbe5b072b0c64490f63
sva777 <vasya.sokolov@mail.ru>
2023-03-14 00:02:45 +0300

    task number 2 completed

--- main.py
+++ main.py
@@ -18,7 +18,25 @@ class Student:
                 lecturer.grades[course] = grade
         else:
             return 'Ошибка'
+    def avg_grade_course(list_student, course):
+        sum_gr = 0
+        count_grade = 0
+        for it in list_student:
+            for cr, gr in it.grades.items():
+                if cr == course:
+                    count_grade += len(gr)
+                    sum_gr += sum(gr)
+        if count_grade != 0:
+            return round(sum_gr / count_grade, 2)
+        return ""
+    def avg_grade (self):
+        avg_list = []
+        for i in self.grades.values():
+            avg_list += i
+        return sum(avg_list) / len(avg_list)
 
+        # sum(list(i[0]) / len(list(i[0])
+            # / len(self.grades.values())
     # and grade in self.range_grades
     # def add_courses(self, course_name):
     #     self.finished_courses.append(course_name)
@@ -43,18 +61,16 @@ class Lecturer(Mentor):
     def __str__(self):
         print(f'Имя: {self.name} \nФамилия: {self.surname}')
     #
-    def avg_grade(list_lecturer, course):
+    def avg_grade_course(list_lecturer, course):
         sum_gr = 0
         count_grade = 0
         for it in list_lecturer:
             for cr, gr in it.grades.items():
-                if cr != course:
-                    continue
-                else:
+                if cr == course:
                     count_grade += len(gr)
                     sum_gr += sum(gr)
         if count_grade != 0:
-            return sum_gr / count_grade
+            return round(sum_gr / count_grade, 2)
         return ""
 
 class Reviewer (Mentor):
@@ -75,10 +91,12 @@ class Reviewer (Mentor):
 best_student = Student('Ruoy', 'Eman', 'man')
 best_student2 = Student('Pac', 'Man', 'man')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Sql']
+
 
 coll_reviewer = Reviewer('Vasya','Sokolov')
 coll_reviewer.courses_attached += ['Python']
-
+coll_reviewer.courses_attached += ['Sql']
 #
 cool_lecturer = Lecturer('Vova','Ivanov')
 cool_lecturer.courses_attached += ['Python']
@@ -87,34 +105,25 @@ cool_lecturer2.courses_attached += ['Python']
 
 best_student.rate_lecturer(cool_lecturer, 'Python', [1,2,4,5])
 best_student.rate_lecturer(cool_lecturer2, 'Python', [3,2,4,10])
+coll_reviewer.rate_hw(best_student, 'Python', 5)
 # coll_reviewer.rate_hw(best_student, 'Python', 10)
+coll_reviewer.rate_hw(best_student, 'Sql', 8)
+
 # coll_reviewer.rate_hw(best_student, 'Python', 15)
 # coll_reviewer.rate_hw(best_student, 'Sql', 5)
 # coll_reviewer.rate_hw(best_student, 'Sql', 12)
 
-# print(best_student.grades)
+print(best_student.grades)
 # print(cool_lecturer.grades)
 # coll_reviewer.__str__()
 # cool_lecturer.__str__()
 
 # print(Student.list_all_student)
 # print(Lecturer.list_all_lecturer)
-#
-# for obj in Lecturer.list_all_mentor:
-#     print(obj.grades)
 
-# def avg_grade(list_lecturer, course):
-#     sum_gr = 0
-#     count_grade = 0
-#     for it in list_lecturer:
-#         for cr, gr in it.grades.items():
-#             if cr != course:
-#                 continue
-#             else:
-#                 count_grade += len(gr)
-#                 sum_gr += sum(gr)
-#     if count_grade != 0:
-#         return sum_gr / count_grade
-#     return ""
 
-print(Lecturer.avg_grade(Lecturer.list_all_lecturer, 'Python'))
+
+
+# print(Lecturer.avg_grade(Lecturer.list_all_lecturer, 'Python'))
+print(Student.avg_grade_course(Student.list_all_student, 'Sql'))
+print(best_student.avg_grade())
